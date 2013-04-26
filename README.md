41597cee8688431be8c54d3b03d1114950f1b7dd
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2013-04-27 00:12:15 +0200

    Initial Release

---
+++ README.md
@@ -0,0 +1,40 @@
+# TwigPress: A boilerplate Twig Theme Engine for Wordpress #
+
+## What is TwigPress? ##
+TwigPress is a Wordpress theme which is based upon Twig Engine. Probably it could be flagged also as "another Twig Engine Theme for Wordpress".
+
+## So, why you think it is special? ##
+TwigPress was born with ease in mind, cleaner APIs and ease to extended. Well, kind-of for now :)
+
+## API ##
+In fact there's not much to explain. When you want to run TwigPress in your template file you just have to do something like
+  
+<code>
+<?php  
+  new TwigPress(array(/* your arguments here */));  
+?>
+</code>
+
+and the arguments you can use until now are:
+
+### 0.1 ###
+* `vars`: your custom template variables you could use on your .twig templates within the `tplapi` keyworkd.
+
+## Template API ##
+TwigPress comes with three main API keywords:
+
+### 0.1 ###
+* `wpapi` this is the most wonderful API which TwigPress comes in mind with. It is the entry point to access Wordpress Core APIs. So any `wpapi.your_api_call_here` is exactly you was used to do with templates until now.
+
+* `twpapi` this is the main TwigPress API entry point of the next future releases. For now you'll have just these APIs:
+    * `base_uri` this is the base URL which points to your theme installation inside the "templates/" directory.
+    * `template_name` this is the current template name which Wordpress required.
+    * `fn` this will be the entry point to custom function of TwigPress. Until now they are:
+        * `addCss` this is a custom function to enqueue your CSS in Wordpress. Useful for other plugins if you want let them to compact your CSS for example.
+        * `addJs` same as `addCss` but this time for Javascript files.
+
+* `tplapi` this will be your custom vars that you'll pass to TwigPress when you initialize your Class.
+
+## License ##
+This project is covered up by **GPLv3** license. You can find it attached to any .php file and the LICENSE file itself.
+Please keep it open as much as you can :)
---
+++ functions.php
@@ -0,0 +1,120 @@
+<?php
+    /*
+    TwigPress is a boilerplate Twig Engine for Wordpress.
+    Copyright (C) 2013 Julian Xhokaxhiu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Need help? Contact me at http://about.me/JulianXhokaxhiu
+    */
+
+    require_once __DIR__.'/core/lib/Twig/Autoloader.php';
+    Twig_Autoloader::register();
+
+    /* DEBUG ONLY */
+    define('TwigPress_Debug', true);
+    define('TwigPress_Template_URI', get_template_directory_uri().'/templates');
+
+    class WP_API_Proxy{
+        /*
+            Description: Proxy Wordpress APIs so they could be available to Twig without mapping them.
+            Based upon: Darko Goles (http://inchoo.net/wordpress/twig-wordpress-part2/)
+        */
+        public function __call($function,$arguments) {
+            if (!function_exists($function)) {
+                trigger_error('['.get_class(self).'] '.'function ['.$function.'] do not exist. Is it a Wordpress API?',E_USER_ERROR);
+                return NULL;
+            }
+            return call_user_func_array($function,$arguments);
+        }
+    }
+
+    class TwigPress_API_Proxy{
+        public function __call($function,$arguments) {
+            if (!function_exists($function)) {
+                trigger_error('['.get_class(self).'] '.'function ['.$function.'] do not exist. Is it a Wordpress API?',E_USER_ERROR);
+                return NULL;
+            }
+            return call_user_func_array($function,$arguments);
+        }
+        /* APIs */
+        public function addCss($path){
+            $hash = md5(TwigPress_Template_URI.$path);
+            wp_register_style($hash,TwigPress_Template_URI.$path);
+            wp_enqueue_style($hash);
+        }
+        public function addJs($path){
+            $hash = md5(TwigPress_Template_URI.$path);
+            wp_register_script($hash,TwigPress_Template_URI.$path);
+            wp_enqueue_script($hash);
+        }
+    }
+
+    class TwigPress{
+        protected $apis;
+        protected $twig;
+        function __construct($args = array()){
+            $loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
+            $this->twig = new Twig_Environment($loader, array(
+                'cache' => TwigPress_Debug ? false : __DIR__.'/cache',
+                'debug' => TwigPress_Debug
+            ));
+            if(TwigPress_Debug) $this->twig->addExtension(new Twig_Extension_Debug());
+            $this->render(get_current_template(),$args['vars']);
+        }
+        protected function render($name,$tpl_vars = array()){
+            if(isset($name)){
+                $this->set_base_api('fn', new TwigPress_API_Proxy());
+                $this->set_base_api('template_name',$name);
+
+                $template = $this->twig->loadTemplate($name.'.twig');
+                echo $template->render($this->init_apis($tpl_vars));
+            }else $this->display_error('No template specified.');
+        }
+        protected function init_apis($tpl_vars = array()){
+            $core_vars = array(
+                "twpapi" => $this->get_base_api(),
+                "wpapi" => new WP_API_Proxy(),
+                "tplapi" => $tpl_vars
+            );
+
+            return $core_vars;
+        }
+        protected function get_base_api(){
+            $this->set_base_api('base_uri',TwigPress_Template_URI);
+            return $this->apis;
+        }
+        protected function set_base_api($key,$value){
+            $this->apis[$key] = $value;
+        }
+        /* UTILITY */
+        protected function display_error($error){
+            trigger_error('['.get_class($this).'] '.$error,E_USER_ERROR);
+        }
+    }
+
+    /*
+        Description: get the current requested template
+        URL: http://wordpress.stackexchange.com/questions/10537/get-name-of-the-current-template-file/10565#10565
+    */
+    add_filter('template_include','var_template_include');
+    function var_template_include($t){
+        $tpl_path = pathinfo($t);
+        $GLOBALS['current_theme_template'] = $tpl_path['filename'];
+        return $t;
+    }
+    function get_current_template(){
+        if(!isset($GLOBALS['current_theme_template'])) return false;
+        return $GLOBALS['current_theme_template'];
+    }
